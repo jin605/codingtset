81d383cbb6d649748350b0e500081b0cef3e3a75
jin605 <jinddd3@gmail.com>
2026-01-26 14:58:52 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.04 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -4,22 +4,20 @@ class Solution {
         
         int n = board.length;
         int count = 0;
-        int[] dh = {0, 1, -1, 0};
+        int[] dh = {0, 1 ,-1 ,0};
         int[] dw = {1, 0, 0, -1};
         
-        for(int i = 0; i <= 3; i++){
+        for (int i = 0; i <= 3; i++){
             int h_check = h + dh[i];
             int w_check = w + dw[i];
-            if (h_check >= 0 && h_check < n){
-                if (w_check >= 0 && w_check < n){
-                    if (board[h][w].equals(board[h_check][w_check]))
-                        count ++;
+            if ((w_check >= 0 && w_check < n) &&(h_check >= 0 && h_check < n)){
+                if(board[h][w].equals(board[h_check][w_check])){
+                    count ++;
                 }
             }
-            
-        }
-        answer = count;
+           }
         
+        answer = count;
         return answer;
     }
 }
