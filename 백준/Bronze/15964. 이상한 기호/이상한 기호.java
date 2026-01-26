c61bba9ebf9de9fb5e6bb5c07944df7b9d8db54b
jin605 <jinddd3@gmail.com>
2026-01-27 08:47:00 +0900

    [Bronze V] Title: 이상한 기호, Time: 108 ms, Memory: 14176 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -8,12 +9,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
 
-        int result = (a + b) * (a -b);
+        long result = (a + b) * (a -b);
         System.out.println(result);
 
 
