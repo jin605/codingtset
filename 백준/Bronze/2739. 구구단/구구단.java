7de072a0056ae2ebc62204fa98bd5f626d220dca
jin605 <jinddd3@gmail.com>
2026-01-27 23:17:59 +0900

    [Bronze V] Title: 구구단, Time: 116 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -4,17 +4,17 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main (String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 1; i <= 9; i ++){
-            System.out.println(n + " * " + i + " = " + n*i);
+    int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i < 10; i++) {
+            System.out.printf("%d * %d = %d\n", n, i, n * i);
+
         }
 
-
     }
-
 
 }
