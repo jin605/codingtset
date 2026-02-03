ecbd3d1386f9316487563a3e3f0ec5e71fbb1d3c
jin605 <jinddd3@gmail.com>
2026-02-03 14:10:21 +0900

    [Bronze II] Title: 공 바꾸기, Time: 108 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,36 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[] x = new int[n];
+        int[] a = new int[n];
+        for (int i = 0; i <n ; i++) {
+            a[i] = i+1;
 
-        for (int i = 0; i < x.length; i++) {
-            x[i] = i + 1;
         }
+
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int idx1 = Integer.parseInt(st.nextToken());
             int idx2 = Integer.parseInt(st.nextToken());
-            int temp = x[idx1-1];
-            x[idx1-1] = x[idx2-1];
-            x[idx2-1] = temp;
+
+            int temp = a[idx1-1];
+            a[idx1-1] = a[idx2-1];
+            a[idx2-1] = temp;
         }
 
-        for (int i = 0; i < x.length ; i++) {
-            System.out.printf("%d ",x[i]);
+        for (int i : a) {
+            sb.append(i).append(" ");
         }
+        System.out.println(sb);
+
 
 
     }
