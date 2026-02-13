1e4cdf06a163d41ee74f03cf978beee8567b89eb
jin605 <jinddd3@gmail.com>
2026-02-13 22:45:13 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 216 ms, Memory: 15264 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -5,19 +5,19 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
+        for (int i = 0; i < n; i++){
+
+            for (int j = 0; j < n - i -1 ;j++){
                 System.out.print(" ");
             }
-            for (int k = 1; k <= i; k++) {
+            for (int k = 0; k <= i; k++ ){
                 System.out.print("*");
             }
             System.out.println();
         }
-
-
 
 
 
